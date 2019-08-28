615ef6bac8b44a97e6d942e5b8ff83a8c37c561a
Joey Brown <joeyrb93@gmail.com>
2019-08-28 09:52:47 -0600

    fix description semantics

--- mdc.py
+++ mdc.py
@@ -8,16 +8,16 @@ Joey Brown
 September 22, 2017
 
 ::Module Description::
-Pattern Classification using Minimum Distance Classification
+Minimum Distance Classification for Pattern Classification
 This module reads in a CSV file and groups each class into a 
 list of features mapped in a N-dimensional plane. Once all data
 has been read in to 'train' the program the final entry will be entered
 and the closest distance to the centroid of an already known class will
 be used as the new entry's class type.
 The CSV file format must follow a pattern similar to below
-TITLE, #=CLASSNAME1, #=CLASSNAME2, ..., #=CLASSNAMEX
-SAMPLE, CLASS, FEATURE1, FEATURE2, ..., FEATUREX
-DATA1, DATA2, ...
+TITLE, #=CLASSNAME_1, #=CLASSNAME_2, ..., #=CLASSNAME_n
+SAMPLE, CLASS, FEATURE_1, FEATURE_2, ..., FEATURE_n
+DATA_1, DATA_2, ...
 """
 import sys
 import CSVIO
